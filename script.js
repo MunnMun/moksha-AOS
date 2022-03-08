1fcad810ca4b87ca9ec5ed2a1443077bf8144eec
Harsh kaushik <97521192+MunnMun@users.noreply.github.com>
2022-03-09 01:12:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+AOS.init();
+
+const counters = document.querySelectorAll('.counter');
+const speed = 40;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const inc = target / speed;
+
+        if (count < target) {
+            counter.innerText = count + inc;
+
+            setTimeout(updateCount,50);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
